c81179345cd4ab62d9ca85b4352994e499037c3a
prat9009 <pratyush.gaurav@accenture.com>
2026-04-20 11:23:14 +0530

    Add User model class

---
+++ octofit/models/user.py
@@ -0,0 +1,8 @@
+class User:
+    def __init__(self, id, name, email):
+        self.id = id
+        self.name = name
+        self.email = email
+
+    def __repr__(self):
+        return f"User(id={self.id}, name='{self.name}', email='{self.email}')"
