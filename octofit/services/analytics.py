c4a1c4b3039341d4f2de1fe17d8d641dcb90315e
prat9009 <pratyush.gaurav@accenture.com>
2026-04-20 11:24:55 +0530

    Add Analytics service class

---
+++ octofit/services/analytics.py
@@ -0,0 +1,15 @@
+class Analytics:
+    def __init__(self):
+        pass
+
+    def track_event(self, event_name, properties=None):
+        # logic to track event
+        pass
+
+    def set_user_properties(self, user_id, properties):
+        # logic to set user properties
+        pass
+
+    def flush(self):
+        # flush the events
+        pass
